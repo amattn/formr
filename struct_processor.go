b13f0129308d2f767f7a87ce4b8c9e03d2455fbb
amattn <amattn@users.noreply.github.com>
2018-03-22 15:52:51 -0700

    refactor and support for bool

--- struct_processor.go
+++ struct_processor.go
@@ -14,12 +14,18 @@ func init() {
 
 }
 
-// Not a stable interface yet.  I expect this funciton signature to change eventually.
-func ProcessStruct(some_struct interface{}) ([]string, error) {
-	form_output := []string{}
+type FormElement struct {
+	Label   string
+	Element string
+}
+
+// Not a stable interface yet.  I expect this function signature to change eventually.
+func ProcessStruct(some_struct interface{}) ([]FormElement, error) {
+	form_output := []FormElement{}
 
 	thing_type := reflect.TypeOf(some_struct)
 	thing_kind := thing_type.Kind()
+	thing_value := reflect.ValueOf(some_struct)
 
 	if thing_kind != reflect.Struct {
 		derr := deeperror.New(417084840, "ProcessStruct() expected struct got "+fmt.Sprintf("%T", some_struct), nil)
@@ -36,10 +42,29 @@ func ProcessStruct(some_struct interface{}) ([]string, error) {
 
 		switch field_kind := struct_field.Type.Kind(); {
 		case field_kind == reflect.String:
-			form_output = append(form_output, form_output_string(struct_field))
+			current_value := thing_value.Field(i)
+			unwrapped_value := current_value.String()
+			form_field, err := form_output_string(struct_field, unwrapped_value)
+			if err != nil {
+				all_errors = append(all_errors, err)
+			} else {
+				form_output = append(form_output, form_field)
+			}
 
 		case field_kind == reflect.Int64:
-			form_field, err := form_output_int64(struct_field)
+			current_value := thing_value.Field(i)
+			unwrapped_value := current_value.Int()
+			form_field, err := form_output_int64(struct_field, unwrapped_value)
+			if err != nil {
+				all_errors = append(all_errors, err)
+			} else {
+				form_output = append(form_output, form_field)
+			}
+
+		case field_kind == reflect.Bool:
+			current_value := thing_value.Field(i)
+			unwrapped_value := current_value.Bool()
+			form_field, err := form_output_bool(struct_field, unwrapped_value)
 			if err != nil {
 				all_errors = append(all_errors, err)
 			} else {
@@ -58,16 +83,16 @@ const (
 	STRUCT_TAG_KEY_FIELD_ID          = "s2w_id"
 	STRUCT_TAG_KEY_FIELD_NAME        = "s2w_name"
 	STRUCT_TAG_KEY_LABEL_CONTENTS    = "s2w_label"
-	STRUCT_TAG_KEY_DEFAULT_VALUE     = "s2w_value" // TODO
+	// STRUCT_TAG_KEY_DEFAULT_VALUE     = "s2w_value" // TODO
+	STRUCT_TAG_KEY_CURRENT_VALUE = "s2w_value"
 )
 
 // need field_id, field_name, label_contents
-func form_output_string(struct_field reflect.StructField) string {
+func form_output_string(struct_field reflect.StructField, current_value string) (FormElement, error) {
 
 	form_field_id := struct_field.Tag.Get(STRUCT_TAG_KEY_FIELD_ID)
 	form_field_name := struct_field.Tag.Get(STRUCT_TAG_KEY_FIELD_NAME)
 	label_contents := struct_field.Tag.Get(STRUCT_TAG_KEY_LABEL_CONTENTS)
-	default_value := struct_field.Tag.Get(STRUCT_TAG_KEY_DEFAULT_VALUE)
 
 	if form_field_id == "" {
 		form_field_id = "PLACEHOLDER"
@@ -80,21 +105,25 @@ func form_output_string(struct_field reflect.StructField) string {
 	}
 
 	// TODO consider switch to a template??
-	return fmt.Sprintf("<label for=\"%s\">%s</label><input type=\"text\" id=\"%s\" class=\"s2w_%s\" name=\"%s\" value=\"%s\">",
+
+	label_string := fmt.Sprintf("<label for=\"%s\">%s</label>",
 		form_field_id,
-		label_contents,
+		label_contents)
+
+	element_string := fmt.Sprintf("<input type=\"text\" id=\"%s\" class=\"s2w_%s\" name=\"%s\" value=\"%s\">",
 		form_field_id,
 		struct_field.Type.String(),
 		form_field_name,
-		default_value)
+		current_value)
+
+	return FormElement{Label: label_string, Element: element_string}, nil
 }
 
-func form_output_int64(struct_field reflect.StructField) (string, error) {
+func form_output_int64(struct_field reflect.StructField, current_value int64) (FormElement, error) {
 
 	form_field_id := struct_field.Tag.Get(STRUCT_TAG_KEY_FIELD_ID)
 	form_field_name := struct_field.Tag.Get(STRUCT_TAG_KEY_FIELD_NAME)
 	label_contents := struct_field.Tag.Get(STRUCT_TAG_KEY_LABEL_CONTENTS)
-	default_value := struct_field.Tag.Get(STRUCT_TAG_KEY_DEFAULT_VALUE)
 
 	if form_field_id == "" {
 		form_field_id = "PLACEHOLDER"
@@ -107,17 +136,72 @@ func form_output_int64(struct_field reflect.StructField) (string, error) {
 	}
 
 	data := map[string]interface{}{
-		STRUCT_TAG_KEY_FIELD_ID:       form_field_id,
-		STRUCT_TAG_KEY_FIELD_NAME:     form_field_name,
-		STRUCT_TAG_KEY_LABEL_CONTENTS: label_contents,
-		STRUCT_TAG_KEY_DEFAULT_VALUE:  default_value,
+		STRUCT_TAG_KEY_STRUCT_FIELD_TYPE: struct_field.Type.String(),
+		STRUCT_TAG_KEY_FIELD_ID:          form_field_id,
+		STRUCT_TAG_KEY_FIELD_NAME:        form_field_name,
+		STRUCT_TAG_KEY_LABEL_CONTENTS:    label_contents,
+		STRUCT_TAG_KEY_CURRENT_VALUE:     current_value,
 	}
-	log.Println("3327233781 data", data)
-	raw_template := `<label for="{{ .s2w_id }}">{{ .s2w_label }}</label><input type="text" id="{{ .s2w_id }}" class="s2w_{{ .s2w_type }}" name="{{ .s2w_name }}" value="{{ .s2w_value }}">`
 
-	tmpl, err := template.New("form_output_int64").Parse(raw_template)
+	label_template := `<label for="{{ .s2w_id }}">{{ .s2w_label }}</label>`
+	element_template := `<input type="text" id="{{ .s2w_id }}" class="s2w_{{ .s2w_type }}" name="{{ .s2w_name }}" value="{{ .s2w_value }}">`
+
+	return execute_templates(1522519197, label_template, element_template, data)
+}
+
+func form_output_bool(struct_field reflect.StructField, current_value bool) (FormElement, error) {
+
+	form_field_id := struct_field.Tag.Get(STRUCT_TAG_KEY_FIELD_ID)
+	form_field_name := struct_field.Tag.Get(STRUCT_TAG_KEY_FIELD_NAME)
+	label_contents := struct_field.Tag.Get(STRUCT_TAG_KEY_LABEL_CONTENTS)
+
+	if form_field_id == "" {
+		form_field_id = "PLACEHOLDER"
+	}
+	if form_field_name == "" {
+		form_field_name = form_field_id
+	}
+	if label_contents == "" {
+		label_contents = form_field_name
+	}
+
+	is_checked := ""
+	if current_value {
+		is_checked = "checked"
+	}
+
+	data := map[string]interface{}{
+		STRUCT_TAG_KEY_STRUCT_FIELD_TYPE: struct_field.Type.String(),
+		STRUCT_TAG_KEY_FIELD_ID:          form_field_id,
+		STRUCT_TAG_KEY_FIELD_NAME:        form_field_name,
+		STRUCT_TAG_KEY_LABEL_CONTENTS:    label_contents,
+		STRUCT_TAG_KEY_CURRENT_VALUE:     is_checked,
+	}
+
+	label_template := `<label for="{{ .s2w_id }}">{{ .s2w_label }}</label>`
+	element_template := `<input type="checkbox" id="{{ .s2w_id }}" class="s2w_{{ .s2w_type }}" name="{{ .s2w_name }}" {{if .s2w_value }}{{ .s2w_value }}{{end}}>`
+
+	return execute_templates(1963919951, label_template, element_template, data)
+}
+
+func execute_templates(debug_num int64, label_template, element_template string, data interface{}) (FormElement, error) {
+	label_string, err := execute_single_template(debug_num, label_template, data)
 	if err != nil {
-		derr := deeperror.New(727509799, "", err)
+		return FormElement{}, err
+	}
+
+	element_string, err := execute_single_template(debug_num, element_template, data)
+	if err != nil {
+		return FormElement{}, err
+	}
+
+	return FormElement{Label: label_string, Element: element_string}, nil
+}
+
+func execute_single_template(debug_num int64, raw_template string, data interface{}) (string, error) {
+	tmpl, err := template.New(fmt.Sprintf("%d", debug_num)).Parse(raw_template)
+	if err != nil {
+		derr := deeperror.New(debug_num, "Parse failure", err)
 		derr.AddDebugField("raw_template", raw_template)
 		return "", derr
 	}
@@ -125,7 +209,7 @@ func form_output_int64(struct_field reflect.StructField) (string, error) {
 	buff := new(bytes.Buffer)
 	err = tmpl.Execute(buff, data)
 	if err != nil {
-		derr := deeperror.New(3396615818, "", err)
+		derr := deeperror.New(debug_num, "Execute failure", err)
 		derr.AddDebugField("raw_template", raw_template)
 		derr.AddDebugField("data", data)
 		return "", derr
--- struct_processor_test.go
+++ struct_processor_test.go
@@ -33,10 +33,10 @@ func TestStruct(t *testing.T) {
 }
 
 type ComplexStruct struct {
-	ShortString string  `s2w_id:"uname" s2w_name:"user_name" s2w_label:"Name:" s2w_value:"Bobber McTester"`
+	ShortString string  `s2w_id:"uname" s2w_name:"user_name" s2w_label:"Name:"`
 	LongString  string  `s2w_id:"ustory" s2w_name:"story" s2w_label:"Story:" `
-	BigInt      int64   `s2w_id:"age_id" s2w_name:"age_name" s2w_label:"Age:" s2w_value:"42"`
-	Float       float64 `s2w_id:"ratio_id" s2w_name:"ratio_name" s2w_label:"Ratio:" s2w_value:"1.55" `
+	BigInt      int64   `s2w_id:"age_id" s2w_name:"age_name" s2w_label:"Age:"`
+	Float       float64 `s2w_id:"ratio_id" s2w_name:"ratio_name" s2w_label:"Ratio:"`
 	TrueOrFalse bool
 
 	DateTime    time.Time
@@ -45,6 +45,11 @@ type ComplexStruct struct {
 
 func TestComplexStruct(t *testing.T) {
 	is_a_struct := ComplexStruct{}
+	is_a_struct.ShortString = "Bobber McTester"
+	is_a_struct.LongString = "This is not my story"
+	is_a_struct.BigInt = 42
+	is_a_struct.Float = 1.55
+	is_a_struct.TrueOrFalse = true
 	form_strings, err := ProcessStruct(is_a_struct)
 	if err != nil {
 		t.Errorf("1198170734 %s type:%T desc:%v", "Expected err != nil but it seems to be nil", is_a_struct, is_a_struct)
@@ -61,4 +66,52 @@ func TestComplexStruct(t *testing.T) {
 			t.Logf("%d %s", i, form_element)
 		}
 	}
+}
+
+type CheckboxStruct struct {
+	StartTrue  bool
+	StartFalse bool
+	FilledOut  bool `s2w_id:"cool_id" s2w_name:"cool_name" s2w_label:"Are we cool?" `
+}
+
+func TestCheckbox(t *testing.T) {
+
+	is_a_struct := CheckboxStruct{}
+	is_a_struct.StartTrue = true
+	is_a_struct.FilledOut = true
+
+	form_elements, err := ProcessStruct(is_a_struct)
+	if err != nil {
+		t.Errorf("3366803382 %s type:%T desc:%v", "Expected err != nil but it seems to be nil", is_a_struct, is_a_struct)
+	}
+
+	some_type := reflect.TypeOf(is_a_struct)
+	if len(form_elements) != some_type.NumField() {
+		t.Errorf("Expected %d form elements of output, but got: %d", some_type.NumField(), len(form_elements))
+		t.Log("Output Lines:")
+		for i, form_element := range form_elements {
+			t.Logf("%d %s", i, form_element)
+		}
+	}
+
+	label_expecteds := make([]string, 3)
+	element_expecteds := make([]string, 3)
+
+	label_expecteds[0] = `<label for="PLACEHOLDER">PLACEHOLDER</label>`
+	label_expecteds[1] = `<label for="PLACEHOLDER">PLACEHOLDER</label>`
+	label_expecteds[2] = `<label for="cool_id">Are we cool?</label>`
+
+	element_expecteds[0] = `<input type="checkbox" id="PLACEHOLDER" class="s2w_bool" name="PLACEHOLDER" checked>`
+	element_expecteds[1] = `<input type="checkbox" id="PLACEHOLDER" class="s2w_bool" name="PLACEHOLDER" >`
+	element_expecteds[2] = `<input type="checkbox" id="cool_id" class="s2w_bool" name="cool_name" checked>`
+
+	for i, form_element := range form_elements {
+		if form_element.Label != label_expecteds[i] {
+			t.Errorf("328727756 %d %s\n%s\n%s\n%s", i, "Expected:", label_expecteds[i], "     Got:", form_element.Label)
+		}
+		if form_element.Element != element_expecteds[i] {
+			t.Errorf("328727757 %d %s\n%s\n%s\n%s", i, "Expected:", element_expecteds[i], "     Got:", form_element.Element)
+		}
+	}
+
 }
